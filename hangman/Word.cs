59b1259764a3187705e4943a26be6558b682b16b
REAPIX\Reapism <reapsprgm@gmail.com>
2019-01-20 22:21:52 -0500

    Added the ability to have the words load from a textfile, and load through the static constructor.

--- hangman/Word.cs
+++ hangman/Word.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace hangman
 {
     public class Word
     {
+        private static List<string> WordList = new List<string>();
+
         /* We can add new words to WordList below.
          * Each time the player starts a new game,
          * a random word is taken from WordList.
@@ -23,10 +26,23 @@ namespace hangman
             }
         }
 
-        private string theWord;
+        private string theWord;   
         public string TheWord
         {
             get { return theWord; }
+        }
+        
+        /*
+         * Static constructor for loading all
+         * the words into the static List<string> 
+         * WordList.
+         */ 
+
+        static Word() {
+            StreamReader sr = new StreamReader("word_list.txt");
+            while (!sr.EndOfStream) {
+                WordList.Add(sr.ReadLine());
+            }
         }
 
         public void LoadWord()
@@ -34,15 +50,7 @@ namespace hangman
             Random rnd = new Random();
             int num = rnd.Next(0, WordList.Count);
             theWord = WordList[num];
-        }
-
-
-        /* Add words below */
-        private static List<string> WordList = new List<string>() {
-            "CHRISTMAS" , "PLEASE", "MUSIC","HOPE","SOFTWARE",
-            "COMPUTER","MISTAKE","POSITIVE","SECRET","WORD",
-            "SCIENTIST","SUBSTANCE","WOODPECKER","SCRABBLE"
-        };
+        }                 
         
     }
 }
